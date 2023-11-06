2b037a6d875df3dab5b5d2c9d90129278f2b7231
HustleHardgg <115645737+HustleHardgg@users.noreply.github.com>
2023-11-06 23:05:34 +0400

    Delete src directory

--- src/components/ProjectTitle/ProjectTitle.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ProjectTitle.css";
-
-function ProjectTitle({ title }) {
-  return <h2 className="project__title">{title}</h2>;
-}
-
-export default ProjectTitle;
